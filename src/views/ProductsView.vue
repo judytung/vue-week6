79db9d01cc23713e4dd6339ace6b29ec7e2a91d4
TUNG HSIAO HAN <sky250331@gmail.com>
2022-03-01 20:55:32 +0800

    取得產品列表

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,3 +1,31 @@
 <template>
   <h2>產品列表</h2>
+  {{products}}
 </template>
+
+<script>
+import axios from 'axios'
+// import vueAxios from 'vue-axios'
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    getProducts () {
+      const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`
+      axios.get(url)
+        .then(res => {
+          this.products = res.data.products
+        })
+        .catch((err) => {
+          alert(err.data.message)
+        })
+    }
+  },
+  mounted () {
+    this.getProducts()
+  }
+}
+</script>
