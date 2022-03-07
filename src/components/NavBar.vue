4f942aba44fc676bd0affdba2e6e8ad7a725e480
TUNG HSIAO HAN <sky250331@gmail.com>
2022-03-07 20:11:05 +0800

    add: logo 、 components.scss、mixin.scss、nav.scss

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Own Dessert</router-link>
+      <h1>
+        <router-link class="logo navbar-brand d-block" to="/">Own Dessert</router-link>
+      </h1>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
