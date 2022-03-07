15592d51207dc38a7b4c89347bcbf60cdeb2d115
TUNG HSIAO HAN <sky250331@gmail.com>
2022-03-07 19:04:16 +0800

    fix: icon 引入方式 測試利用變數更換顏色

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,14 +21,10 @@
           </li>
         </ul>
       </div>
-      <button type="button" class="btn btn-primary position-relative" @click="changeCart">
-        <span >
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-          <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-          </svg>
-        </span>
-        <span class="badge rounded-pill bg-danger">{{ cartData.carts.length }}</span>
-      </button>
+        <a class="btn btn-primary position-relative bg-light border-0" @click="changeCart">
+          <i class="bi bi-cart2 fs-4"></i>
+          <span class="badge rounded-pill bg-danger position-absolute start-50">{{ cartData.carts.length }}</span>
+        </a>
     </div>
   </nav>
 </template>
@@ -53,9 +49,9 @@ export default {
           this.cartData = res.data.data // data 裡有兩層，要存到最後一個 data
           emitter.emit('get-cart')
         })
-        .catch((err) => {
-          alert(err.data.message)
-        })
+        // .catch((err) => {
+        //   alert(err.data.message)
+        // })
     },
     changeCart () {
       this.$router.push('/cart')
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
